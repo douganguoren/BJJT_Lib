02b8dce70ea687546865272e136ebe33b76a7426
1240019656@qq.com <1240019656@qq.com>
2016-02-24 14:12:14 +0800

    新建核心库
    新建核心库

---
+++ BJJT_Lib/LoadData/RequestInfo.m
@@ -0,0 +1,13 @@
+//
+//  RequestInfo.m
+//  BJJT_KEY_iOS
+//
+//  Created by 程三 on 16/2/18.
+//  Copyright (c) 2016年 张博林. All rights reserved.
+//
+
+#import "RequestInfo.h"
+
+@implementation RequestInfo
+
+@end
